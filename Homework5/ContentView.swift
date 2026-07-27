213407c65ceec6e1bb39218e37951625de611e9c
Guofeng Luo <luoguofeng0401@gmail.com>
2026-07-28 05:18:06 +0800

    Initial Commit

---
+++ Homework5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Homework5
+//
+//  Created by Guofeng Luo on 2026/7/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Homework5/Homework5App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework5App.swift
+//  Homework5
+//
+//  Created by Guofeng Luo on 2026/7/28.
+//
+
+import SwiftUI
+
+@main
+struct Homework5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
