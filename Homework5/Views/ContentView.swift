3f5e6d4666450778a21b499e712b85e5739c1267
Guofeng Luo <luoguofeng0401@gmail.com>
2026-07-28 06:48:48 +0800

    add tabview

--- Homework5/Views/ContentView.swift
+++ Homework5/Views/ContentView.swift
@@ -11,19 +11,33 @@ struct ContentView: View {
     let data = ItemData.data
     
     var body: some View {
-        NavigationStack {
-            List {
-                ForEach(data) { category in
-                    NavigationLink {
-                        ItemList(items: category.items)
-                    } label: {
-                        CategoryRow(category: category)
-                    }
+        TabView {
+            Tab("Items", systemImage: "list.bullet") {
+                NavigationStack {
+                    List {
+                        ForEach(data) { category in
+                            NavigationLink {
+                                ItemList(items: category.items)
+                            } label: {
+                                CategoryRow(category: category)
+                            }
 
+                        }
+                    }
+                    .navigationTitle("Categories")
                 }
             }
-            .navigationTitle("Categories")
+            
+            Tab("Documents", systemImage: "square.stack.3d.up") {
+                List {
+                    Text("Documents")
+                    Text("Certificates")
+                    Text("Passport")
+                    Text("Cash")
+                }
+            }
         }
+        
     }
 }
 
